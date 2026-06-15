edaae9624e9bf6a5e019588291e61d21698c7939
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-06-15 17:35:22 -0300

    organização dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import './styles/theme.css';
 import './styles/global.css';
 
+
 import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá, Mundo!{' '}
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-        exercitationem quisquam omnis excepturi nam veritatis in quas itaque ab,
-        numquam iste veniam adipisci autem sapiente quibusdam at illo
-        consequatur velit. Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Explicabo ratione cum vitae, voluptates labore aspernatur? Quos
-        voluptatum debitis dolorum, provident dolores id illum, aliquid nisi
-        aspernatur ut consectetur optio libero.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+      <Container>
+        <Heading>FORM</Heading>
+      </Container>
+      <Container>
+        <Heading>FOOTER</Heading>
+      </Container>
     </>
   );
 }

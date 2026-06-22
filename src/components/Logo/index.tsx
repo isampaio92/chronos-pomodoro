bd60c8f713f6bde2c8d5c29c7b7551a578f6fa49
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-06-22 14:26:00 -0300

    criação do componente de Logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import { TimerIcon } from 'lucide-react';
+import styles from './styles.module.css';
+
+
+export function Logo() {
+  return <div className={`${styles.logo} ${styles.purple}`}>
+    <a className={styles.logoLink} href="#">
+      <TimerIcon />
+      <span>Chronos</span>
+    </a>
+  </div>;
+}

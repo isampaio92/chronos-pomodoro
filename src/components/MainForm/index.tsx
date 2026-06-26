95f8d169fd8172e759ac42740c398af2af3a3a0a
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-06-26 17:04:26 -0300

    iniciando a lógica no formulário

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,18 +1,62 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import styles from './styles.module.css';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!taskNameInput.current) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa!');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: 1, //Conferir
+        secondsRemaining, //Conferir
+        formattedSecondsRemaining: '00:00', //Conferir
+        config: { ...prevState.config },
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
-    <form className={styles.form} action=''>
+    <form onSubmit={handleCreateNewTask} className={styles.form} action=''>
       <div className={styles.formRow}>
         <DefaultInput
           labelText='task'
           id='input'
           type='text'
           placeholder='placeholder'
+          ref={taskNameInput}
         />
       </div>
 
@@ -26,7 +70,6 @@ export function MainForm() {
 
       <div className={styles.formRow}>
         <DefaultButton icon={<PlayCircleIcon />} />
-        <DefaultButton icon={<StopCircleIcon />} color='red' />
       </div>
     </form>
   );

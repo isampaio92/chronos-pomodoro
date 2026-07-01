ea1104246d7d11f9c6b4eead1cdf5182685aa24b
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-07-01 13:55:32 -0300

    cria notificações com toastify

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -10,6 +10,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 import { Tips } from '../Tips';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -21,13 +22,14 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss();
 
     if (!taskNameInput.current) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert('Digite o nome da tarefa!');
+      showMessage.warn('Digite o nome da tarefa!');
       return;
     }
 
@@ -42,12 +44,16 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+
+    showMessage.success('Tarefa iniciada');
   }
 
   function handleInterruptTask(
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) {
     e.preventDefault();
+    showMessage.dismiss()
+    showMessage.error('Tarefa interrompida')
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 

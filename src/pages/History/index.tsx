4fa7797f4d659768b9d906cbbdf1311852cdc98b
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-07-01 17:41:16 -0300

    cria a ordenação do histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -6,9 +6,12 @@ import { MainTemplate } from '../../templates/MainTemplate';
 import styles from './styles.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
+import { sortTasks } from '../../utils/sortTasks';
 
 export function History() {
   const { state } = useTaskContext();
+  const sortedTasks = sortTasks({tasks: state.tasks})
 
   return (
     <MainTemplate>
@@ -40,14 +43,20 @@ export function History() {
             </thead>
 
             <tbody>
-              {state.tasks.map((task) => {
+              {sortedTasks.map(task => {
+                const taskTypeDictionary = {
+                  workTime: 'Foco',
+                  shortBreakTime: 'Descanso curto',
+                  longBreakTime: 'Descanso longo',
+                };
+
                 return (
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
                     <td>{formatDate(task.startDate)}</td>
-                    <td>{task.interruptDate}</td>
-                    <td>{task.type}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
                   </tr>
                 );
               })}
